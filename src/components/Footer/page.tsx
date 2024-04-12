b3957a115000eadcc5ec7bc59a384016e4adecea
Gabrielle <gabrielle.castro@ymonetize.com>
2024-04-12 10:21:05 -0300

    at6

--- src/components/Footer/page.tsx
+++ src/components/Footer/page.tsx
@@ -3,12 +3,6 @@ import styles from "./page.module.scss";
 export default function Footer() {
   return (
     <div className={styles.footer}>
-      <div className={styles.footerInfo}>
-        <a href="#">Política de privacidade</a>
-        <p>-</p>
-        <a href="#">Contato</a>
-      </div>
-
       <p className={styles.copy}>2024 @Copyright - Vilella</p>
     </div>
   );
